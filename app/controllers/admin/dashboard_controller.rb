cef0492add146b59f28fc890cf906fe55fa95d55
Danny Jiang <dannyjiang111@gmail.com>
2022-06-15 01:32:50 +0000

    add stats to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
-  
+
   def show
+    @total_quantity = Product.sum(:quantity)
+    @number_categories = Category.distinct.count(:name)
   end
 end
